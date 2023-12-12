956c6788b5fe69e8c75ba40cb2b21f4d035c512f
fbaltaci <fbaltaci@advisor360.com>
2023-12-11 21:57:33 -0500

    turkey culture and place is modified

--- src/components/turkey_thecountry/Turkey_Culture.vue
+++ src/components/turkey_thecountry/Turkey_Culture.vue
@@ -2,16 +2,18 @@
   <section class="turkey-culture">
     <h2 class="turkey-culture__title">Welcome to Turkey!</h2>
     <h1 class="turkey-culture__subtitle">Culture</h1>
-    <p class="turkey-culture__description">
-      Turkish culture is a rich amalgam of influences from the Ottoman Empire,
-      Mediterranean, Middle East, and modern European culture. Turkey is known
-      for its hospitality, vibrant festivals, and diverse traditions.
-    </p>
+    <p class="turkey-culture__description">{{ turkeyCultureDesc }}</p>
   </section>
 </template>
 
 <script>
 export default {
-  data() {},
+  data() {
+    return {
+      turkeyCultureDesc: `Turkish culture is a rich amalgam of influences from the Ottoman Empire,
+        Mediterranean, Middle East, and modern European culture. Turkey is known
+        for its hospitality, vibrant festivals, and diverse traditions.`,
+    };
+  },
 };
 </script>
--- src/components/turkey_thecountry/Turkey_Places.vue
+++ src/components/turkey_thecountry/Turkey_Places.vue
@@ -1,16 +1,14 @@
 <template>
   <section class="turkey-places">
-    <h2 class="turkey-places__title">Nice Places to Visit</h2>
+    <h2 class="turkey-places__title">Nice Places to Visit in Turkey</h2>
     <ul>
-      <li>Istanbul: A city that straddles two continents.</li>
-      <li>
-        Cappadocia: Famous for its unique rock formations and hot air balloon
-        rides.
-      </li>
-      <li>Ephesus: An ancient city known for its ruins.</li>
-      <li>
-        Pamukkale: Known for its thermal waters and terraces of carbonate
-        minerals.
+      <li
+        v-for="place in places"
+        :key="place.name"
+        @click="toggleDetails(place)"
+      >
+        {{ place.name }}
+        <p v-if="place.showDetails">{{ place.description }}</p>
       </li>
     </ul>
   </section>
@@ -18,6 +16,39 @@
 
 <script>
 export default {
-  data() {},
+  data() {
+    return {
+      places: [
+        {
+          name: "Istanbul",
+          description:
+            "A city that straddles two continents, rich in history and culture.",
+          showDetails: false,
+        },
+        {
+          name: "Cappadocia",
+          description:
+            "Famous for its unique rock formations and hot air balloon rides.",
+          showDetails: false,
+        },
+        {
+          name: "Ephesus",
+          description: "An ancient city known for its well-preserved ruins.",
+          showDetails: false,
+        },
+        {
+          name: "Pamukkale",
+          description:
+            "Known for its thermal waters and terraces of carbonate minerals.",
+          showDetails: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    toggleDetails(place) {
+      place.showDetails = !place.showDetails;
+    },
+  },
 };
 </script>
